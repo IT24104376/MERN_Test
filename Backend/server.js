dd50a032f5e4663e0ded2c5dedd6306f6ed4a1ba
Kasun Akalanka <upekkasun2003@gmail.com>
2026-04-25 12:55:19 +0530

    cors fix

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'https://mern-test-steel.vercel.app/' || 'localhost:3000' }));
 app.use(express.json());
 
 mongoose
